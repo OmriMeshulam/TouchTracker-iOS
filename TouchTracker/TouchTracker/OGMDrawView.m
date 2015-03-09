dc0f3c74500f3e39487ff0530179bc91af71a1e9
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-08 21:15:21 -0400

    Handling multiple touches and canceled touch. Chapter 12 Complete.

--- TouchTracker/TouchTracker/OGMDrawView.m
+++ TouchTracker/TouchTracker/OGMDrawView.m
@@ -11,7 +11,7 @@
 
 @interface OGMDrawView ()
 
-@property (nonatomic, strong) OGMLine *currentLine;
+@property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
 
 @end
@@ -23,8 +23,10 @@
     self = [super initWithFrame:frame];
     
     if(self){
+        self.linesInProgress = [[NSMutableDictionary alloc]init];
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
+        self.multipleTouchEnabled = YES;
     }
     
     return self;
@@ -32,33 +34,63 @@
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    UITouch *t = [touches anyObject];
+    //Let's put a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
     
-    // Get location of the touch in view's coordinate system
-    CGPoint location =  [t locationInView:self];
-    
-    self.currentLine = [[OGMLine alloc]init];
-    self.currentLine.begin = location;
-    self.currentLine.end = location;
+    for (UITouch *t in touches){
+        CGPoint location  = [t locationInView:self];
+        
+        OGMLine *line = [[OGMLine alloc]init];
+        line.begin = location;
+        line.end = location;
+        
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        self.linesInProgress[key] = line;
+    }
     
     [self setNeedsDisplay];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    UITouch *t = [touches anyObject];
-    CGPoint location = [t locationInView:self];
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
     
-    self.currentLine.end = location;
+    for (UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        OGMLine *line = self.linesInProgress[key];
+        
+        line.end = [t locationInView:self];
+    }
     
     [self setNeedsDisplay];
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    [self.finishedLines addObject:self.currentLine];
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
     
-    self.currentLine = nil;
+    for(UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        OGMLine *line = self.linesInProgress[key];
+        
+        [self.finishedLines addObject:line];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.linesInProgress removeObjectForKey:key];
+    }
     
     [self setNeedsDisplay];
 }
@@ -82,11 +114,11 @@
         [self strokeLine:line];
     }
     
-    if(self.currentLine){
-        // If there is a line currently bieng drawn, do it in red
-        [[UIColor redColor] set];
-        [self strokeLine:self.currentLine];
+    [[UIColor redColor]set];
+    for(NSValue *key in self.linesInProgress){
+        [self strokeLine:self.linesInProgress[key]];
     }
+
 }
 
 @end
