7afc9d6c0fe8f6077d884ee0d8919835b35440de
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-10 18:01:42 -0400

    Added delete UIMenuButton, tap, longPress, green highlight select. Upto pg 257.

--- TouchTracker/TouchTracker/OGMDrawView.m
+++ TouchTracker/TouchTracker/OGMDrawView.m
@@ -14,6 +14,8 @@
 @property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
 
+@property (nonatomic, weak) OGMLine *selectedLine;
+
 @end
 
 @implementation OGMDrawView
@@ -33,6 +35,16 @@
         doubleTapRecognizer.numberOfTapsRequired = 2;
         doubleTapRecognizer.delaysTouchesBegan = YES; // To get rid of touches began dot
         [self addGestureRecognizer:doubleTapRecognizer];
+        
+        UITapGestureRecognizer *tapRecognizer =
+            [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
+        
+        UITapGestureRecognizer *pressRecognizer =
+            [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPress:)];
+        [self addGestureRecognizer:pressRecognizer];
     }
     
     return self;
@@ -44,6 +56,53 @@
     
     [self.linesInProgress removeAllObjects];
     [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+}
+
+- (void)tap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    
+    if(self.selectedLine){
+        
+        // Make ourselves the target of the menu item action messages
+        [self becomeFirstResponder];
+        
+        // Grab the menu controller
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        
+        // Create a new "Delete" UIMenuItem
+        UIMenuItem *deleteItem = [[UIMenuItem alloc]initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        
+        menu.menuItems = @[deleteItem];
+        
+        // Tell the menu where it should come from and show it
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    }else{
+        // Hide the menu if no line is selected
+        [[UIMenuController sharedMenuController]setMenuVisible:NO animated:YES];
+    }
+    
+    [self setNeedsDisplay];
+    
+}
+
+- (void)longPress:(UIGestureRecognizer *)gr
+{
+    if(gr.state == UIGestureRecognizerStateBegan){
+        CGPoint point = [gr locationInView:self];
+        self.selectedLine = [self lineAtPoint:point];
+        
+        if(self.selectedLine){
+            [self.linesInProgress removeAllObjects];
+        }
+    }else if(gr.state == UIGestureRecognizerStateEnded){
+        self.selectedLine = nil;
+    }
     [self setNeedsDisplay];
 }
 
@@ -121,6 +180,38 @@
     [bp stroke];
 }
 
+-(OGMLine *)lineAtPoint:(CGPoint)p
+{
+    // Find a line close to p
+    for (OGMLine *l in self.finishedLines){
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        
+        // Check a few points on the line
+        for (float t = 0.0; t <= 1.0; t += 0.05){
+            float x = start.x + t * (end.x - start.x);
+            float y = end.x + t * (end.y - start.y);
+            
+            // If the tapped point is within 20 points, let's return this line
+            if(hypot(x - p.x, y - p.y) < 20.0){
+                return l;
+            }
+        }
+    }
+    
+    // If nothing is close enough to the tapped point, then  we did not select  a line
+    return nil;
+}
+
+- (void)deleteLine:(id)sender
+{
+    // Remove the selected line from the list of _finishedLines
+    [self.finishedLines removeObject:self.selectedLine];
+    
+    // Redraw everything
+    [self setNeedsDisplay];
+}
+
 - (void)drawRect:(CGRect)rect
 {
     // Draw finished lines in black
@@ -133,7 +224,17 @@
     for(NSValue *key in self.linesInProgress){
         [self strokeLine:self.linesInProgress[key]];
     }
+    
+    if(self.selectedLine){
+        [[UIColor greenColor]set];
+        [self strokeLine:self.selectedLine];
+    }
 
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
 }
 
 @end
