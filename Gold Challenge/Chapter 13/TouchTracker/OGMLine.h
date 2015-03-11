8bbfa8710973fa3e05527211ea07f21c1595863d
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-11 16:38:22 -0400

    Chapter 13 Gold Challenge e: Speed and Size. (recording velocity of the pan and adjusting line thickness based on this speed.)

---
+++ Gold Challenge/Chapter 13/TouchTracker/OGMLine.h
@@ -0,0 +1,18 @@
+//
+//  OGMLine.h
+//  TouchTracker
+//
+//  Created by Omri Meshulam on 3/8/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface OGMLine : NSObject
+
+@property (nonatomic) CGPoint begin;
+@property (nonatomic) CGPoint end;
+@property (nonatomic) float width;
+
+@end
---
+++ Gold Challenge/Chapter 13/TouchTracker/OGMLine.m
@@ -0,0 +1,13 @@
+//
+//  OGMLine.m
+//  TouchTracker
+//
+//  Created by Omri Meshulam on 3/8/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import "OGMLine.h"
+
+@implementation OGMLine
+
+@end
